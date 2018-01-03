44fb3a9f368c0a56fd4fdf1c4f06962148c30e6d
Kipngetich33 <khalifngeno@gmail.com>
2018-01-03 09:34:04 -0500

    register the main blue print in the app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,5 +7,9 @@ def create_app(config_name):
     #creating the app configurations
     app.config.from_object(config_options[config_name]) 
 
+    #registering the blueprints
+    from . main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 
     return app
