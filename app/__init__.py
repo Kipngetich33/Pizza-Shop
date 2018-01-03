27b287485aac82b26f542ce1eb27fd70dca79e65
Kipngetich33 <khalifngeno@gmail.com>
2018-01-03 10:11:10 -0500

    install flask-bootsrap extension and initialize it in the __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
+
+#generating instances
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -10,6 +14,9 @@ def create_app(config_name):
     #registering the blueprints
     from . main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    #intializing flask extensions
+    bootstrap.init_app(app)
 
 
     return app
